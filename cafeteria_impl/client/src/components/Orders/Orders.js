bbff6a17580f8a0bab7fba25b869a0849c0a2d60
Mohammed slimani <slimani.med.21@gmail.com>
2019-05-19 02:03:29 +0100

    Added the Order List Component

--- cafeteria_impl/client/src/components/Orders/Orders.js
+++ cafeteria_impl/client/src/components/Orders/Orders.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import { List, ListItem, ListItemAvatar, Avatar } from '@material-ui/core';
 
 export class Orders extends Component {
+  constructor(props){
+    super(props);
+    //The props are going to contain the Order List that The user has shosen
+  }
   render() {
+    const {MyOrder} =this.props; 
     return (
-      <div>
-        These are your orders
-      </div>
+      <>
+      <List>
+        {MyOrder.map(item=> //TODO: Implement the ListHere!
+          <ListItem button>
+            <ListItemAvatar>
+              <Avatar alt="avatar" src=""/>
+            </ListItemAvatar>
+          </ListItem>
+          )
+        }
+      </List>
+      </>
     )
   }
 }
