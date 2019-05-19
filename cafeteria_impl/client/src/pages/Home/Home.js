e678b89bbc288167d94c29847e33d590b23cb137
Mohammed slimani <slimani.med.21@gmail.com>
2019-05-19 05:12:43 +0100

    Skeleton of the Registration and Login Fields

--- cafeteria_impl/client/src/pages/Home/Home.js
+++ cafeteria_impl/client/src/pages/Home/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import { Grid } from '@material-ui/core';
+import { Login, Register } from '../../components';
 
 export class Home extends Component {
   render() {
     return (
       <>
         <Grid container spacing={12}>
-            <Grid>
-                
+            <Grid item sm={6}>
+                <Login/>
+            </Grid>
+            <Grid item sm={6}>
+                <Register/>
             </Grid>
         </Grid>
       </>
